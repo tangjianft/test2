3e111b3a82341b043a40d06c642cac6a4922f233
tangjianft <105167721@qq.com>
2015-03-15 17:01:39 +0800

    add cache

---
+++ cache/category.php
@@ -0,0 +1,128 @@
+<?php return array (
+  29 => 
+  array (
+    'id' => '29',
+    'parentid' => '0',
+    'name' => '文字路况',
+    'description' => '',
+    'sort' => '50',
+    'url' => '',
+  ),
+  30 => 
+  array (
+    'id' => '30',
+    'parentid' => '0',
+    'name' => '违规曝光',
+    'description' => '',
+    'sort' => '49',
+    'url' => '',
+  ),
+  38 => 
+  array (
+    'id' => '38',
+    'parentid' => '29',
+    'name' => '333',
+    'description' => '333',
+    'sort' => '33',
+    'url' => '33',
+  ),
+  15 => 
+  array (
+    'id' => '15',
+    'parentid' => '0',
+    'name' => '问卷调查',
+    'description' => '11',
+    'sort' => '19',
+    'url' => '',
+  ),
+  18 => 
+  array (
+    'id' => '18',
+    'parentid' => '0',
+    'name' => '礼品奖品',
+    'description' => '',
+    'sort' => '18',
+    'url' => '',
+  ),
+  31 => 
+  array (
+    'id' => '31',
+    'parentid' => '0',
+    'name' => '旅游资讯',
+    'description' => '',
+    'sort' => '15',
+    'url' => '',
+  ),
+  32 => 
+  array (
+    'id' => '32',
+    'parentid' => '0',
+    'name' => '评论信息',
+    'description' => '',
+    'sort' => '14',
+    'url' => '评论信息',
+  ),
+  1 => 
+  array (
+    'id' => '1',
+    'parentid' => '0',
+    'name' => '活动信息',
+    'description' => '',
+    'sort' => '4',
+    'url' => '',
+  ),
+  26 => 
+  array (
+    'id' => '26',
+    'parentid' => '0',
+    'name' => '单页管理',
+    'description' => '',
+    'sort' => '0',
+    'url' => 'index.php?d=admin&c=pages&catid=26',
+  ),
+  27 => 
+  array (
+    'id' => '27',
+    'parentid' => '0',
+    'name' => '广告信息1',
+    'description' => '简介',
+    'sort' => '0',
+    'url' => 'url',
+  ),
+  33 => 
+  array (
+    'id' => '33',
+    'parentid' => '29',
+    'name' => '小分类名称',
+    'description' => '',
+    'sort' => '0',
+    'url' => '',
+  ),
+  34 => 
+  array (
+    'id' => '34',
+    'parentid' => '29',
+    'name' => '奋斗奋斗',
+    'description' => '',
+    'sort' => '0',
+    'url' => '',
+  ),
+  39 => 
+  array (
+    'id' => '39',
+    'parentid' => '27',
+    'name' => '111111111111',
+    'description' => '1111111',
+    'sort' => '0',
+    'url' => '1111111111',
+  ),
+  40 => 
+  array (
+    'id' => '40',
+    'parentid' => '27',
+    'name' => '222FFSDFDS',
+    'description' => '2222222222222',
+    'sort' => '0',
+    'url' => '22222222',
+  ),
+);
